65b79d0c7f235584225af04e0df476bfddc16a1f
Karungg <miftahfadilah71@gmail.com>
2023-10-05 09:40:04 +0700

    UUID

---
+++ tests/Feature/VoucherTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Voucher;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class VoucherTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        DB::delete("DELETE FROM vouchers");
+    }
+
+    public function testCreateVoucher()
+    {
+        $voucher = new Voucher();
+        $voucher->name = "Sample Voucher";
+        $voucher->voucher_code = "123123123123";
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+    }
+
+    public function testCreateVoucherUUID()
+    {
+        $voucher = new Voucher();
+        $voucher->name = "Sample Voucher";
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+        self::assertNotNull($voucher->voucher_code);
+    }
+}
